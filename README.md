2d1277b685c3e79c64f6acb6f8ca40f1ba6e8484
John Alexander <jalexander.qed@gmail.com>
2018-08-31 23:17:42 -0700

    Added React frontend system integrated with Spring Boot

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Website V2
+
+New website is built with Spring Boot (Java) and React
+
+## Credit
+
+Combining Spring Boot and React: [https://github.com/kantega/react-and-spring](https://github.com/kantega/react-and-spring)
---
+++ src/main/java/ninja/example/website/ApiController.java
@@ -0,0 +1,16 @@
+package ninja.example.website;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/api/str/time")
+    public String timeStr() {
+        return (new Date()).toString();
+    }
+
+}
